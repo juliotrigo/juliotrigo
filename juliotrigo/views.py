a147b80f08929457ecd976cb15ea2ea0a2bb28d1
juliotrigo <juliotrigocom@gmail.com>
2013-07-31 23:44:25 +0100

    Bar time.

--- juliotrigo/views.py
+++ juliotrigo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from django.utils.timezone import now
+
 def home(request):
     """Home page."""
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'time': str(now().hour) + ':' + str(now().minute)})
