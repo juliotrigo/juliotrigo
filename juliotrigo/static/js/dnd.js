16ac7dd86fa13bf1f9c21c3a8302390fce1b16c3
juliotrigo <juliotrigocom@gmail.com>
2013-09-27 22:03:52 +0100

    HTML Drag and Drop.

--- juliotrigo/static/js/dnd.js
+++ juliotrigo/static/js/dnd.js
@@ -1,52 +1,125 @@
+/**
+ * Draggable elements.
+ */
+var work = document.getElementById('work');
+var projects = document.getElementById('projects');
+var wrapper = document.getElementById('content-wrapper');
+
+work.style.top = '220px';
+work.style.left = '200px';
+projects.style.top = '320px';
+projects.style.left = '600px';
+
+var internalDNDType = 'text/plain';
+var offset_data;						// Global variable as Chrome doesn't allow access to event.dataTransfer in dragover
+var draggable;							// Element in movement.
+
+/**
+ * Drag n Drop.
+ */
 if (Modernizr.draganddrop) {
 	// Browser supports HTML5 DnD.
 	
-	var obj_work = document.getElementById('work');
-	var obj_projects = document.getElementById('projects');
+	work.addEventListener('dragstart', handleDragStart, false);
+	//work.addEventListener('drag', handleDrag, false);
+	//work.addEventListener('dragend', handleDragEnd, false);
+
+	projects.addEventListener('dragstart', handleDragStart, false);
+	//projects.addEventListener('drag', handleDrag, false);
+	//projects.addEventListener('dragend', handleDragEnd, false);
 	
-	obj_work.addEventListener('dragstart', handleDragStart, false);
-	obj_work.addEventListener('dragenter', handleDragEnter, false);
-	obj_work.addEventListener('dragover', handleDragOver, false);
-	obj_work.addEventListener('dragleave', handleDragLeave, false);
-	obj_work.addEventListener('drop', handleDrop, false);
-	obj_work.addEventListener('dragend', handleDragEnd, false);
-
-	obj_projects.addEventListener('dragstart', handleDragStart, false);
-	obj_projects.addEventListener('dragenter', handleDragEnter, false);
-	obj_projects.addEventListener('dragover', handleDragOver, false);
-	obj_projects.addEventListener('dragleave', handleDragLeave, false);
-	obj_projects.addEventListener('drop', handleDrop, false);
-	obj_projects.addEventListener('dragend', handleDragEnd, false);
+	wrapper.addEventListener('dragenter', handleDragEnter, false);
+	wrapper.addEventListener('dragleave', handleDragLeave, false);
+	wrapper.addEventListener('dragover', handleDragOver, false);
+	wrapper.addEventListener('drop', handleDrop, false);
 	
-} else {
-	// Fallback to a library solution.
 }
 
-function handleDragStart(e) {
-	// this / e.target is the source node.
-
+/**
+ * dragstart
+ */
+function handleDragStart(event) {
+	draggable = event.target.id;
+	
+	var style = window.getComputedStyle(event.target, null);
+	offset_data = (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY);
+	
+	event.dataTransfer.setData(internalDNDType, offset_data);
+	event.dataTransfer.effectAllowed = 'move';
+	event.dataTransfer.dropEffect = 'move';
 }
 
-function handleDragOver(e) {
-
-}
-
-function handleDragEnter(e) {
-	// this / e.target is the current hover target.
-
-}
-
-function handleDragLeave(e) {
-	// this / e.target is previous target element.
-
-}
-
-function handleDrop(e) {
-	// this / e.target is current target element.
+/**
+ * drag
+ */
+function handleDrag(event) {
 
 }
 
-function handleDragEnd(e) {
-	// this/e.target is the source node.
+/**
+ * dragend
+ */
+function handleDragEnd(event) {
+	
+}
 
+/**
+ * dragdrop
+ */
+function handleDrop(event) {
+	var offset;
+
+	try {
+		offset = event.dataTransfer.getData(internalDNDType).split(',');
+	}
+	catch(e) {
+		offset = offset_data.split(',');
+	}
+
+	var dElement = document.getElementById(draggable);
+
+	dElement.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
+	dElement.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
+	
+	if (event.preventDefault) {
+		event.preventDefault();
+	}
+	
+	return false;
+}
+
+/**
+ * dragover
+ */
+function handleDragOver(event) {
+	
+    if (event.preventDefault) {
+		event.preventDefault();
+	}
+    
+    return false;
+}
+
+/**
+ * dragenter
+ */
+function handleDragEnter(event) {
+
+	if (event.preventDefault) {
+		event.preventDefault();
+	}
+	
+	return false;
+}
+
+/**
+ * dragsleave
+ */
+function handleDragLeave(event) {
+
+	if (event.preventDefault) {
+		event.preventDefault();
+	}
+	
+	return false;
 }
