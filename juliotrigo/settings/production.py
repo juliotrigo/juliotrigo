1516d3452dc3e4f753afd056ea21760d0dd15a8b
juliotrigo <juliotrigocom@gmail.com>
2014-01-19 22:29:51 +0000

    ALLOWED_HOSTS

--- juliotrigo/settings/production.py
+++ juliotrigo/settings/production.py
@@ -2,3 +2,8 @@ from juliotrigo.settings.defaults import *
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = [
+    '.juliotrigo.com', # Allow domain and subdomains
+    '.juliotrigo.com.', # Also allow FQDN and subdomains
+]
--- juliotrigo/settings/staging.py
+++ juliotrigo/settings/staging.py
@@ -2,3 +2,7 @@ from juliotrigo.settings.defaults import *
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = [
+    '*', # Allowall  domains and subdomains
+]
