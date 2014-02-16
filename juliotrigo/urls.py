36602ab821716c355b3eed891325bd9fade170f9
juliotrigo <juliotrigocom@gmail.com>
2014-02-16 20:15:24 +0000

    Adding the project version to templates. Style conventions.

--- juliotrigo/urls.py
+++ juliotrigo/urls.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""urls module of the website."""
+
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
--- juliotrigo/views.py
+++ juliotrigo/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
 
+"""Website's views."""
+
+from django.shortcuts import render
 from django.utils.timezone import now, localtime
 
+from juliotrigo import __version__
+
+
 def home(request):
-    """Home page."""
-    bar_time = localtime(now())  # Converts the time in UTC to the current time zone
-    return render(request, 'home.html', {'time': bar_time.strftime("%H:%M")})
+    """Render the home page."""
+    bar_time = localtime(now())  # Converts the UTC time to the current TZ
+    version = __version__
+    return render(request, 'home.html', {'time': bar_time.strftime("%H:%M"),
+                                         'version': version})
